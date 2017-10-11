10aff4b48e68bfd933f83fee0b2f6bd1a9d4f11c
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2017-10-11 01:15:22 -0400

    weather widget basics

--- django/reactjs/components/hello_world.js
+++ django/reactjs/components/hello_world.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+import SearchBar from './search_bar';
+import WeatherList from './weather_list';
+
 class HelloWorld extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <p><strong>Weather</strong></p>
+        <SearchBar />
+        <WeatherList />
+      </div>
     )
   }
 }
---
+++ django/reactjs/components/weather_list.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.name;
+    const temp = Math.round(((cityData.main.temp) * (9 / 5)) - 459.67);
+    const description = cityData.weather[0].description;
+
+    return (
+      <div key={name}>
+        <h1>
+          {name}
+        </h1>
+        <h3>
+          Current Conditions:<br />
+          {temp} &#8457;<br />
+          {description}
+        </h3>
+        <h3>
+
+        </h3>
+      </div>
+
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.weather.map(this.renderWeather)}
+      </div>
+    );
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    weather: state.weather
+  };
+}
+
+export default connect(mapStateToProps)(WeatherList);
