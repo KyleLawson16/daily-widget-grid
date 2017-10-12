ed33bafb859f431cf1c5cb9de744047d5b16ec0d
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2017-10-12 00:16:05 -0400

    updated styling

--- django/reactjs/components/hello_world.js
+++ django/reactjs/components/hello_world.js
@@ -6,10 +6,16 @@ import WeatherList from './weather_list';
 class HelloWorld extends Component {
   render() {
     return (
-      <div>
-        <p><strong>Weather</strong></p>
-        <SearchBar />
-        <WeatherList />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-12 col-md-4">
+            <div className="card">
+              <p><strong>Weather</strong></p>
+              <SearchBar />
+              <WeatherList />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
